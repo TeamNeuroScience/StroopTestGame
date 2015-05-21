631b40ad353df262936c15ad78e9849d6e840f63
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2015-05-21 17:29:53 -0400

    Added methods for fading UI elements in and out.

---
+++ StroopTest/Assets/Scripts/GameController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	//Functions to fade user interface in and out.
+	IEnumerator fadeIn(GameObject obj, float speed) {
+		float increment;
+		obj.SetActive(true);
+		CanvasGroup cv = obj.GetComponent<CanvasGroup>();
+		while (cv.alpha < 1) {
+			increment = speed * Time.deltaTime;
+			if (cv.alpha + increment > 1) cv.alpha = 1;
+			else cv.alpha += speed * Time.deltaTime;
+			yield return null;
+		}
+	}
+	
+	IEnumerator fadeOut(GameObject obj, float speed) {
+		float increment;
+		CanvasGroup cv = obj.GetComponent<CanvasGroup>();
+		while (cv.alpha > 0) {
+			increment = speed * Time.deltaTime;
+			if (cv.alpha - increment < 0) cv.alpha = 0;
+			else cv.alpha -= speed * Time.deltaTime;
+			yield return null;
+		}
+		obj.SetActive(false);
+	}
+}
