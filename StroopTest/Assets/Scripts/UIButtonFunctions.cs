0efb526ca7780cea61bac997f7b6bec7906e8e0c
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2015-05-21 19:54:18 -0400

    Add buttons and a script to handle them.
    (Script is filled with stubs for now)

---
+++ StroopTest/Assets/Scripts/UIButtonFunctions.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIButtonFunctions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void onClickExit()
+    {
+        Application.Quit();
+    }
+
+    public void onClickInstructions()
+    {
+        //TODO: Implement me plz ty
+    }
+
+    public void onClickNewTest()
+    {
+        //TODO: Implement me plz ty
+    }
+}
+
+
